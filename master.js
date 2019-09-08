ab88d3c457c2a51e124b51d8e7a03bba3aa8603b
riyad12 <mangekujang2@gmail.com>
2019-09-08 18:30:19 +0700

    Update master.js

--- master.js
+++ master.js
@@ -18,7 +18,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);var x=Math.floor((Math.random()*5)+1);var xxx=null;if(x=="1"){xxx="/2018/12/manufacturing-erp-software-12-vendors.html"}
+{var encryptedUrl=Base64.encode(a_to_vi);var x=Math.floor((Math.random()*5)+1);var xxx=null;if(x=="1"){xxx="/2019/09/manufacturing-erp-software.html"}
 if(x=="2"){xxx="/2018/12/how-to-develop-it-strategy-to-guide.html"}
 if(x=="3"){xxx="/2018/12/8-secrets-to-developing-business-case.html"}
 if(x=="4"){xxx="/2018/12/6-benefits-of-cloud-erp.html"}
